225d917c63156c30e6a0b5a27c64ee03b7013d73
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-31 14:10:06 +0200

    sylovani, podle mediaQuery, animace GSAP-GreenSock obrazky a odkazy

---
+++ index.js
@@ -0,0 +1,11 @@
+
+gsap.from(".boxik", {duration: 2, opacity: 0, scale: 0.5, ease: "back"});
+
+//gsap.to(".box", {duration: 3, rotation: 360, scale: 0.5});
+
+
+
+
+gsap.from(".tutorial", {duration: 2, opacity: 0, scale: 0.5, ease: "back", delay: 2});
+gsap.from(".play", {duration: 2, opacity: 0, scale: 0.5, ease: "back", delay: 2});
+
